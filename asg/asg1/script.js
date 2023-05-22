d8242ba5c9ef823179cb160d6bd657a7a26b3a23
Ryo-Ura <urawaryosuke@gmail.com>
2023-05-22 14:43:57 -0700

    implemented minimal req

--- asg/asg1/script.js
+++ asg/asg1/script.js
@@ -1,66 +1,74 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Get references to HTML elements
-  const form = document.getElementById('item-form');
-  const itemNameInput = document.getElementById('item-name');
-  const itemDescriptionInput = document.getElementById('item-description');
-  const itemPriceInput = document.getElementById('item-price');
-  const itemImageInput = document.getElementById('item-image');
-  const itemList = document.getElementById('item-list');
-  const deleteAllBtn = document.getElementById('delete-all-btn');
 
-  // Add event listeners
-  form.addEventListener('submit', addItem);
-  deleteAllBtn.addEventListener('click', deleteAllItems);
+const itemForm = document.getElementById('itemForm');
+const itemList = document.getElementById('item-list');
+const deleteAllBtn = document.getElementById('delete-all-btn');
 
-  // Item list array
-  let items = [];
+let items = [];
+var initialItemsString = '[{"name":"Item 1","description":"Description 1","price":10,"imageURL":"https://slp-statics.astockcdn.net/static_assets/staging/22spring/free/browse-vector-categories-collections/Card4_399895799.jpg?width=580"}]';
+items = JSON.parse(initialItemsString);
 
-  // Handle form submission
-  function addItem(event) {
-    event.preventDefault();
 
-    // Retrieve input values
-    const itemName = itemNameInput.value;
-    const itemDescription = itemDescriptionInput.value;
-    const itemPrice = itemPriceInput.value;
-    const itemImage = itemImageInput.value;
+itemForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent form submission and page reload
 
-    // Create an object with item details
-    const newItem = {
-      name: itemName,
-      description: itemDescription,
-      price: itemPrice,
-      image: itemImage
-    };
+  // Retrieve form field values
+  console.log(event)
+  const itemName = document.getElementById('itemName');
+  const description = document.getElementById('description');
+  const price = document.getElementById('price');
+  const imageURL = document.getElementById('imageURL');
 
-    // Append the new item to the list
-    items.push(newItem);
+  var newItem = {
+    name: itemName.value,
+    description: description.value,
+    price: parseFloat(price.value),
+    imageURL: imageURL.value
+  };
 
-    // Generate HTML markup for item card
-    const itemCard = createItemCard(newItem);
-
-    // Append item card to the list container
-    itemList.appendChild(itemCard);
-
-    // Clear form inputs
-    clearFormInputs();
-  }
-
-  // Create HTML markup for item card
-  function createItemCard(item) {
-    // Generate the necessary markup for an item card using item properties
-    // Return the generated HTML markup
-  }
-
-  // Clear form inputs
-  function clearFormInputs() {
-    // Clear the values of form inputs
-  }
-
-  // Delete all item cards
-  function deleteAllItems() {
-    // Remove all item cards from the DOM
-    // Clear the items array
-  }
-
+  items.push(newItem);
+  displayItem(newItem);
 });
+
+deleteAllBtn.addEventListener('click', () => {
+  itemList.innerHTML = '';
+  items = [];
+});
+
+function displayItem(item) {
+  const itemList = document.getElementById('item-list');
+
+  // Create a card element
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const itemName = document.createElement('h2');
+  itemName.textContent = item.itemName;
+  card.appendChild(itemName);
+
+  const description = document.createElement('p');
+  description.textContent = item.description;
+  card.appendChild(description);
+
+  const price = document.createElement('p');
+  const priceValue = parseFloat(item.price); // Convert price to a number
+  price.textContent = `Price: $${priceValue.toFixed(2)}`;
+  card.appendChild(price);
+
+  const image = document.createElement('img');
+  image.src = item.imageURL;
+  card.appendChild(image);
+
+  itemList.appendChild(card);
+}
+
+function initializePage() {
+  items.forEach(function(item) {
+    displayItem(item);
+  });
+}
+
+window.addEventListener('load', initializePage);
+
+function clearForm() {
+  itemForm.reset();
+}
